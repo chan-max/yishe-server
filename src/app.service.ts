1c148f5bd19ad5fed24a2e5a782ada4f64b806c8
chan-max <jackieontheway666@gmail.com>
2025-01-03 10:15:59 +0800

    dev

--- src/app.service.ts
+++ src/app.service.ts
@@ -13,10 +13,10 @@ export class AppService {
   getBasicConfig() {
     return {
       cos: {
-        SecretId: 'AKIDMdmaMD0uiNwkVH0gTJFKXaXJyV4hHmAL',
-        SecretKey: 'HPdigqyzpgTNICCQnK0ZF6zrrpkbL4un',
-        Bucket: '1s-1257307499',
-        Region: 'ap-beijing'
+        // SecretId: 'AKIDMdmaMD0uiNwkVH0gTJFKXaXJyV4hHmAL',
+        // SecretKey: 'HPdigqyzpgTNICCQnK0ZF6zrrpkbL4un',
+        // Bucket: '1s-1257307499',
+        // Region: 'ap-beijing'
       },
       publicKey: this.keyService.getPublicKey()
     }
