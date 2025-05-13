dcfb42ac1850a6c1d50bbdfa899b879bbc8e9a68
chan-max <jackieontheway666@gmail.com>
2025-05-14 07:21:37 +0800

    dev

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 
 # yishe `s new server
+
+
+
+
--- config/env.prod.ts
+++ config/env.prod.ts
@@ -7,9 +7,9 @@ export default {
     type: 'mysql',
     host: '49.232.186.238', // 主机，默认为localhost
     port: 3306, // 端口号
-    username: 'lif', // 用户名
+    username: 'yishe', // 用户名
     password: '666666', // 密码
-    database: 'lif', //数据库名
+    database: 'yishe', //数据库名
     dateStrings: true, // 设置返回日期为字符串
     autoLoadEntities: true, // 使用这个配置自动导入entities
     synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
