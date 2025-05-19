6066c6cb9776e8e107615cc2aa5c54f24e649b37
chan-max <jackieontheway666@gmail.com>
2025-05-19 23:59:11 +0800

    dev

--- config/env.prod.ts
+++ config/env.prod.ts
@@ -7,9 +7,9 @@ export default {
     type: 'mysql',
     host: '49.232.186.238', // 主机，默认为localhost
     port: 3306, // 端口号
-    username: 'cz', // 用户名
+    username: 's1', // 用户名
     password: '666666', // 密码
-    database: 'design', //数据库名
+    database: 's1', //数据库名
     dateStrings: true, // 设置返回日期为字符串
     autoLoadEntities: true, // 使用这个配置自动导入entities
     synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
--- config/env.ts
+++ config/env.ts
@@ -5,11 +5,11 @@ export default {
   // 数据库配置
   DATABASE_CONFIG: {
     type: 'mysql',
-    host: 'localhost', // 主机，默认为localhost
+    host: '49.232.186.238', // 主机，默认为localhost
     port: 3306, // 端口号
-    username: 'root', // 用户名
-    password: '666666z.', // 密码
-    database: 'design', //数据库名
+    username: 's1', // 用户名
+    password: '666666', // 密码
+    database: 's1', //数据库名
     dateStrings: true, // 设置返回日期为字符串
     autoLoadEntities: true, // 使用这个配置自动导入entities
     synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
@@ -17,7 +17,7 @@ export default {
   // redis配置
   REDIS: {
     port: 6379, //Redis 端口
-    host: '127.0.0.1', //Redis 域名
+    host: '49.232.186.238', //Redis 域名
     db: 0,
     family: 4,
     password: '123456', //'Redis 访问密码'
