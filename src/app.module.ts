c19d5e16472dad5bbd0a41604eb63dd0b2df7007
chan-max <jackieontheway666@gmail.com>
2025-06-02 17:42:25 +0800

    dev

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,7 +11,6 @@ import { CustomModelModule } from './custom_model/custom_model.module';
 import { CommentModule } from './comment/comment.module';
 import { KeyService } from 'src/utils/key.service';
 import { CompanyModule } from './company/company.module';
-import { ResourceModule } from './resource/resource.module';
 
 // 环境配置信息
 import envConfig from '../config';
@@ -27,7 +26,6 @@ import envConfig from '../config';
     CustomModelModule,
     CommentModule,
     CompanyModule,
-    ResourceModule
   ],
   controllers: [AppController],
   providers: [AppService, KeyService],
