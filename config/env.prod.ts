14c221b54813788e5094db546d6c2699c9e2deca
chan-max <jackieontheway666@gmail.com>
2025-06-02 17:48:26 +0800

    dev

--- config/env.prod.ts
+++ config/env.prod.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: chan-max jackieontheway666@gmail.com
+ * @Date: 2025-05-20 06:12:19
+ * @LastEditors: chan-max jackieontheway666@gmail.com
+ * @LastEditTime: 2025-06-02 17:47:28
+ * @FilePath: /design-server/config/env.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 export default {
   https:true,
   // token密钥
@@ -7,9 +15,9 @@ export default {
     type: 'mysql',
     host: '49.232.186.238', // 主机，默认为localhost
     port: 3306, // 端口号
-    username: 'yishe', // 用户名
+    username: 's1', // 用户名
     password: '666666', // 密码
-    database: 'yishe', //数据库名
+    database: 's1', //数据库名
     dateStrings: true, // 设置返回日期为字符串
     autoLoadEntities: true, // 使用这个配置自动导入entities
     synchronize: true, //根据实体自动创建数据库表， 生产环境建议关闭
@@ -17,9 +25,11 @@ export default {
   // redis配置
   REDIS: {
     port: 6379, //Redis 端口
-    host: 'localhost', //Redis 域名
+    host: '49.232.186.238', //Redis 域名
+    // host: 'localhost', //Redis 域名
     db: 0,
     family: 4,
-    password: '', //'Redis 访问密码'
+    password: '666666', //'Redis 访问密码'
+    lazyConnect:true,
   },
 };
