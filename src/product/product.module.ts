c6ae9907741583b462f904cfa1318f96f9b7e303
chan-max <jackieontheway666@gmail.com>
2025-06-05 00:21:50 +0800

    dev

--- src/product/product.module.ts
+++ src/product/product.module.ts
@@ -8,13 +8,17 @@
  */
 
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Product } from './entities/product.entity';
 import { ProductService } from './product.service';
 import { ProductController } from './product.controller';
-import { Product } from './entities/product.entity';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from '../common/common.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Product])],
+  imports: [
+    TypeOrmModule.forFeature([Product]),
+    CommonModule,
+  ],
   controllers: [ProductController],
   providers: [ProductService],
   exports: [ProductService],
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -11,12 +11,14 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { Product } from './entities/product.entity';
 import { BasicService } from 'src/common/basicService';
 import { In } from 'typeorm';
+import { CosService } from '../common/cos.service';
 
 @Injectable()
 export class ProductService extends BasicService {
   constructor(
     @InjectRepository(Product)
     private productRepository,
+    private cosService: CosService,
   ) {
     super();
   }
@@ -37,6 +39,27 @@ export class ProductService extends BasicService {
     const item = await this.productRepository.findOne(post.id);
     if (!item) {
       throw new Error('商品不存在');
+    }
+
+    // 处理图片更新
+    if (post.images !== undefined) {
+      const oldImages = item.images || [];
+      const newImages = post.images || [];
+      
+      // 找出被删除的图片
+      const deletedImages = oldImages.filter(url => !newImages.includes(url));
+      
+      // 删除不再使用的 COS 文件
+      if (deletedImages.length > 0) {
+        try {
+          await Promise.all(
+            deletedImages.map(url => this.cosService.deleteFile(url))
+          );
+        } catch (error) {
+          console.error('删除 COS 文件失败:', error);
+          // 这里我们不抛出错误，因为文件删除失败不应该影响商品更新
+        }
+      }
     }
 
     // 只更新允许的字段
