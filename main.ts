b57241acdc42c102b4ef7ce207c0972ff11d0a34
chan-max <jackieontheway666@gmail.com>
2025-06-04 08:12:34 +0800

    dev

--- main.ts
+++ main.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: chan-max jackieontheway666@gmail.com
+ * @Date: 2025-06-02 17:09:08
+ * @LastEditors: chan-max jackieontheway666@gmail.com
+ * @LastEditTime: 2025-06-04 08:11:58
+ * @FilePath: /design-server/main.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import * as express from 'express';
