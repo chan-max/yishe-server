e533773698fd8c30a9544d0fe168e762473f13d2
chan-max <jackieontheway666@gmail.com>
2025-06-09 21:59:52 +0800

    dev

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,7 +2,7 @@
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2025-05-20 06:12:19
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2025-05-25 09:27:00
+ * @LastEditTime: 2025-06-09 21:58:45
  * @FilePath: /design-server/src/app.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -29,6 +29,11 @@ export class AppController {
   @Get('hello')
   public hello() {
     return 'hello 1s!'
+  }
+
+  @Get('test')
+  public test() {
+    return true
   }
 
   
