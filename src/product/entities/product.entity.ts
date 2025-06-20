c77945e94ffa7a9ae9649ba38b769d69abfc506b
chan-max <jackieontheway666@gmail.com>
2025-06-20 08:38:23 +0800

    dev

--- src/product/entities/product.entity.ts
+++ src/product/entities/product.entity.ts
@@ -2,7 +2,7 @@
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2025-05-24 12:42:30
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2025-06-20 08:17:43
+ * @LastEditTime: 2025-06-20 08:38:14
  * @FilePath: /design-server/src/product/entities/product.entity.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
