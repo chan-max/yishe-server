9da690faff02ec2c07690396cb90cdf91ae0171f
chan-max <jackieontheway666@gmail.com>
2025-07-08 23:48:02 +0800

    dev

--- src/custom_model/custom_model.service.ts
+++ src/custom_model/custom_model.service.ts
@@ -2,7 +2,7 @@
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2025-06-02 17:58:18
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2025-06-13 00:34:48
+ * @LastEditTime: 2025-07-08 20:18:09
  * @FilePath: /design-server/src/custom_model/custom_model.service.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -59,6 +59,10 @@ export class CustomModelService extends BasicService {
 
   async update(post) {
     const item = await this.customModelRepository.findOne(post.id);
+    // 如果缩略图有变化，且原来有缩略图，先删除旧的
+    if (post.thumbnail && item.thumbnail && post.thumbnail !== item.thumbnail) {
+      await this.cosService.deleteFile(item.thumbnail);
+    }
     Object.assign(item, post);
     return this.customModelRepository.save(item);
   }
