039f45c386ad262cc4f92004976d5d170e50dc18
chan-max <jackieontheway666@gmail.com>
2025-07-09 21:26:47 +0800

    dev

--- src/draft/draft.controller.ts
+++ src/draft/draft.controller.ts
@@ -2,7 +2,7 @@
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2025-06-12 21:16:22
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2025-06-21 21:09:25
+ * @LastEditTime: 2025-07-09 21:19:52
  * @FilePath: /design-server/src/draft/draft.controller.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -30,7 +30,6 @@ import {
 import { OptionalAuthGuard } from 'src/common/authGuard';
 
 @Controller('draft')
-@ApiTags('草稿箱')
 export class DraftController {
   constructor(private readonly draftService: DraftService) {}
 
