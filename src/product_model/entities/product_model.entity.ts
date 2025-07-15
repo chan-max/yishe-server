264d9df28efe9dc9d52cfc90513c718f02a28d33
chan-max <jackieontheway666@gmail.com>
2025-07-16 06:38:49 +0800

    重构商品列表

--- src/product_model/entities/product_model.entity.ts
+++ src/product_model/entities/product_model.entity.ts
@@ -2,7 +2,7 @@
  * @Author: chan-max jackieontheway666@gmail.com
  * @Date: 2025-06-02 17:58:18
  * @LastEditors: chan-max jackieontheway666@gmail.com
- * @LastEditTime: 2025-07-09 08:26:55
+ * @LastEditTime: 2025-07-16 05:12:49
  * @FilePath: /design-server/src/product_model/entities/product_model.entity.ts
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -47,19 +47,7 @@ export class ProductModel {
     @Column({ length: 1000, default: '', nullable: true })
     keywords: string; // 描述
 
-    @Column({ nullable:true ,default:0 })
-    ref_count: number; // 模型引用次数，有多少模型引用了该模型
-
-    @Column({ nullable:true,default:0 })
-    like_count: number; // 点赞次数
-
     @Column({ nullable:true,default:0  })
-    save_count: number; // 收藏次数
-
-    @Column({ nullable:true,default:0  })
-    link_count: number; //  引用次数
-
-    @Column({ nullable:true,type:'varchar', length: 1000 })
     thumbnail: string; // 缩略图地址 , 用作该模型的封面图 ， 不同于实物图
 
     @Column({ type:'json',nullable:true })
