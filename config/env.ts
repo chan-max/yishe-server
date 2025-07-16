142f0aba42cc9aa111f0cb5f535f385659bd0aef
chan-max <jackieontheway666@gmail.com>
2025-07-16 19:24:41 +0800

    fix env mode

--- config/env.ts
+++ config/env.ts
@@ -7,7 +7,7 @@
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 export default {
-  https:true,
+  https:false,
   // token密钥
   SECRET: 'test123456',
   // 数据库配置
--- config/index.ts
+++ config/index.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: chan-max jackieontheway666@gmail.com
+ * @Date: 2025-06-02 17:51:11
+ * @LastEditors: chan-max jackieontheway666@gmail.com
+ * @LastEditTime: 2025-07-16 19:24:29
+ * @FilePath: /design-server/config/index.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import localEnv from './env';
 import prodEnv from './env.prod';
 
@@ -9,6 +17,7 @@ const parseEnv = {
 
 
 // 生产模式
-const isProd = true
+const isProd = process.env.NODE_ENV === 'production';
+console.log(`[config] 当前环境: ${isProd ? '生产' : '开发'} (NODE_ENV=${process.env.NODE_ENV})`);
 
 export default parseEnv[isProd ? 'production' : 'development'];
